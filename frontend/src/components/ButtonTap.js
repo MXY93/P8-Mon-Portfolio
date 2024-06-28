8665e520e40b2c32bf6029b80f3bd08b6c0dc32c
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-06-29 01:16:18 +0200

    ButtonTap component

---
+++ frontend/src/components/ButtonTap.js
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion";
+import '../styles/buttonTap.scss'
+
+export default function ButtonTap({ imgSrc, alt, text, onClick }) {
+  return (
+    <motion.button
+      className="box"
+      whileHover={{ scale: 1.2 }}
+      whileTap={{ scale: 0.9 }}
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}
+      onClick={onClick}
+    >
+      <img src={imgSrc} alt={alt}/>
+      <p>{text}</p>
+    </motion.button>
+  );
+}
