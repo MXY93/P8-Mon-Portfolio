00da8928a4b44bdb30def9ebbeef994c30586c6e
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-06-30 10:47:58 +0200

    Router deleted for BrowserRouter

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -7,12 +7,10 @@ import Layout from "./components/Layout";
 export default function AppRouter({ themeToggler }) {
   return (
     <BrowserRouter basename='/'>
-      <Router>
         <Routes>
           <Route path="/" element={<Layout themeToggler={themeToggler}><Home /></Layout>} />
           <Route path="*" element={<Layout themeToggler={themeToggler} isErrorPage={true}><NotFound /></Layout>} />
         </Routes>
-      </Router>
     </BrowserRouter>
   );
 }
