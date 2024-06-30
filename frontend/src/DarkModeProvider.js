0b23125a5cce87e8c62b910ca46545e0bf6b2adb
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-06-30 20:52:14 +0200

    Darkmode logic revisited

--- frontend/src/DarkModeProvider.js
+++ frontend/src/DarkModeProvider.js
@@ -1,17 +1,26 @@
-import { createContext, useState } from "react"
+import React, { createContext, useState, useEffect } from "react";
 
 export const DarkModeContext = createContext();
 
 export default function DarkModeProvider({ children }) {
   const [darkMode, setDarkMode] = useState(false);
 
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('darkmode');
+      document.body.classList.remove('lightmode');
+    } else {
+      document.body.classList.add('lightmode');
+      document.body.classList.remove('darkmode');
+    }
+  }, [darkMode]);
+
   function toggleDarkMode() {
-    setDarkMode(!darkMode);
+    setDarkMode(prevMode => !prevMode);
   }
 
   return (
-    <DarkModeContext.Provider 
-    value={ {darkMode, toggleDarkMode} }>
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
       {children}
     </DarkModeContext.Provider>
   );
