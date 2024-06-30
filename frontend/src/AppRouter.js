412d58286633bed59bc69da61e21331bd18c52ee
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-06-30 10:00:32 +0200

    BrowserRouter basename added

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
 
 export default function AppRouter({ themeToggler }) {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout themeToggler={themeToggler}><Home /></Layout>} />
-        <Route path="*" element={<Layout themeToggler={themeToggler} isErrorPage={true}><NotFound /></Layout>} />
-      </Routes>
-    </Router>
+    <BrowserRouter basename='/'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout themeToggler={themeToggler}><Home /></Layout>} />
+          <Route path="*" element={<Layout themeToggler={themeToggler} isErrorPage={true}><NotFound /></Layout>} />
+        </Routes>
+      </Router>
+    </BrowserRouter>
   );
 }
