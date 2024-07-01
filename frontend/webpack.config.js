1c7f6b538df750bc657b7feec71ddddba20ca4c4
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-07-01 18:53:14 +0200

    Fix build configuration and deploy to Vercel

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -93,7 +93,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, 'build'),
     },
     compress: true,
     port: 3000,
